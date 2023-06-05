59b4b0b34c0e82e17aced246170178ae16ed19b1
a00269002_AndreaGaviria <a00269002@mycambrian.ca>
2023-06-05 17:48:58 -0400

    Implemented Interface command

--- Assignment/InterfaceCommand.cs
+++ Assignment/InterfaceCommand.cs
@@ -4,3 +4,33 @@ interface RobotCommand
 {
     void Run(Robot robot); // Interfaces are public and abstract by default.
 }
+
+class OffCommand : RobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = false;
+}
+
+class OnCommand : RobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = true;
+}
+
+class WestCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
+}
+
+class EastCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X++; }
+}
+
+class SouthCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y--; }
+}
+
+class NorthCommand : RobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y++; }
+}
