48fb8acdecae21c30e22b91dfaca662ac60b9128
a00269002_AndreaGaviria <a00269002@mycambrian.ca>
2023-06-06 16:36:53 -0400

    Add test cases and update with Interfaces

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -10,9 +10,49 @@ namespace AssignmentTest
         {
             Robot testRobot = new Robot();
             testRobot.IsPowered = true;
-            Assert.AreEqual(testRobot.IsPowered,true);
+            Assert.AreEqual(testRobot.IsPowered, true);
             testRobot.IsPowered = false;
-            Assert.AreEqual(testRobot.IsPowered,false);
+            Assert.AreEqual(testRobot.IsPowered, false);
+        }
+
+        [TestMethod]
+        public void WestTest()
+        {
+            Robot testRobot = new Robot();
+            testRobot.IsPowered = true;
+            Assert.AreEqual(testRobot.X--, 0);
+            testRobot.IsPowered = false;
+            Assert.AreEqual(testRobot.X--, -1);
+        }
+
+        [TestMethod]
+        public void EastTest()
+        {
+            Robot testRobot = new Robot();
+            testRobot.IsPowered = true;
+            Assert.AreEqual(testRobot.X++, 0);
+            testRobot.IsPowered = false;
+            Assert.AreEqual(testRobot.X++, 1);
+        }
+
+        [TestMethod]
+        public void SouthTest()
+        {
+            Robot testRobot = new Robot();
+            testRobot.IsPowered = true;
+            Assert.AreEqual(testRobot.Y--, 0);
+            testRobot.IsPowered = false;
+            Assert.AreEqual(testRobot.Y--, -1);
+        }
+
+        [TestMethod]
+        public void NorthTest()
+        {
+            Robot testRobot = new Robot();
+            testRobot.IsPowered = true;
+            Assert.AreEqual(testRobot.Y++, 0);
+            testRobot.IsPowered = false;
+            Assert.AreEqual(testRobot.Y++, 1);
         }
     }
 }
