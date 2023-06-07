437fc8e441afed3090c33e9a2ca298a31a038a23
a00269002_AndreaGaviria <a00269002@mycambrian.ca>
2023-06-07 17:06:37 -0400

    Add unit test for robot properties

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -54,5 +54,29 @@ namespace AssignmentTest
             testRobot.IsPowered = false;
             Assert.AreEqual(testRobot.Y++, 1);
         }
+
+        [TestMethod]
+        public void PropertiesTest()
+        {
+            Robot robot1 = new();
+            Assert.AreEqual(robot1.NumCommands, 6);
+            var expectedCommands = 10;
+            Robot robot2 = new(expectedCommands);
+            Assert.AreEqual(robot2.NumCommands, expectedCommands);
+
+            Assert.AreEqual(robot1.IsPowered, false);
+            robot1.IsPowered = true;
+            Assert.AreEqual(robot1.IsPowered, true);
+
+            Assert.AreEqual(robot1.X, 0);
+            // Moves the robot can move even though it is off!!
+            // This is very bad! Not good encapsulation
+            robot1.X = -5;
+            Assert.AreEqual(robot1.X, -5);
+
+            Assert.AreEqual(robot1.Y, 0);
+            robot1.Y = -5;
+            Assert.AreEqual(robot1.Y, -5);
+        }
     }
 }
