8ecab7b6b9482f04bd5bcd8d160e46cf17f1873a
a00269002_AndreaGaviria <a00269002@mycambrian.ca>
2023-06-08 21:50:15 -0400

    Using enum

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,41 +4,61 @@ namespace Assignment
 {
     static class Program
     {
+        enum Commands
+        {
+            on,
+            off,
+            north,
+            south,
+            east,
+            west
+        }
         public static bool AssignCommand(Robot robot, string command)
         {
-            switch (command)
+            if (Enum.TryParse<Commands>(command, out Commands thisCommand))
             {
-                case "on":
-                    robot.LoadCommand(new OnCommand());
-                    break;
-                case "off":
-                    robot.LoadCommand(new OffCommand());
-                    break;
-                case "north":
-                    robot.LoadCommand(new NorthCommand());
-                    break;
-                case "south":
-                    robot.LoadCommand(new SouthCommand());
-                    break;
-                case "east":
-                    robot.LoadCommand(new EastCommand());
-                    break;
-                case "west":
-                    robot.LoadCommand(new WestCommand());
-                    break;
-                default:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid command - Try again");
-                    Console.ResetColor();
-                    return false;
+                switch (thisCommand)
+                {
+                    case Commands.on:
+                        robot.LoadCommand(new OnCommand());
+                        break;
+                    case Commands.off:
+                        robot.LoadCommand(new OffCommand());
+                        break;
+                    case Commands.north:
+                        robot.LoadCommand(new NorthCommand());
+                        break;
+                    case Commands.south:
+                        robot.LoadCommand(new SouthCommand());
+                        break;
+                    case Commands.east:
+                        robot.LoadCommand(new EastCommand());
+                        break;
+                    case Commands.west:
+                        robot.LoadCommand(new WestCommand());
+                        break;
+                }
+                return true;
             }
-            return true;
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid command - Try again");
+                Console.ResetColor();
+                return false;
+            }
         }
         static void Main()
         {
             // Run your RobotTester class here -> RobotTester.TestRobot()
             Robot newRobot = new();
-            Console.WriteLine("Give 6 commands to the robot. Possible commands are: \n on \n off \n north \n south \n east \n west");
+            Console.WriteLine("Give 6 commands to the robot. Possible commands are:");
+            Commands[] allCommands = (Commands[])Enum.GetValues(typeof(Commands));
+
+            foreach (Commands command in allCommands)
+            {
+                Console.WriteLine(command);
+            }
             for (int input = 0; input < 6; input++)
             {
                 Console.Write($"Assign command #{input + 1}: ");
